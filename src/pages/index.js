e0f4e60fe84c7828042d7a751d71e93d57d02a67
Timothy Hoang <timh1203@gmail.com>
2018-04-16 13:39:10 -0400

    Add bootstrap and jumbotron

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from '../components/Header'
 
-export default () => <div>Hello world!</div>
+class Index extends React.Component {
+    render(){
+        return(
+            <Header />
+        )
+    }
+}
+
+export default Index
